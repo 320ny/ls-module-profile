5a4829f4e8ea96883e2bb1b00dcfc268bb11ed0c
Ryan Bennick <ryan@320ny.com>
2012-10-02 23:14:11 -0400

    Introduced a 'on_updateAccountBillingAndShipping' action

--- classes/profile_actions.php
+++ classes/profile_actions.php
@@ -144,4 +144,87 @@ class Profile_Actions extends Cms_ActionScope {
 	
 		Shop_CheckoutData::set_shipping_info($shipping_info);
 	}
+	
+
+	public function on_updateAccountBillingAndShipping($ajax_mode = true) {
+		if($ajax_mode)
+			$this->action();
+
+		try {
+			// ACCOUNT
+			$this->update_account($_POST);
+
+			// BILLING
+			$this->update_billing($_POST);	
+			
+			// SHIPPING
+			$this->update_shipping($_POST);
+
+			if(!post('no_flash', false))
+				Phpr::$session->flash['success'] = "Account updated successfully!";
+
+			$redirect = post('redirect');
+
+			if($redirect)
+				Phpr::$response->redirect($redirect);
+
+		}
+		catch(Exception $ex) {
+			throw new Cms_Exception($ex->getMessage());
+		}
+	}
+
+	private function update_account($post) {
+
+		$this->customer->disable_column_cache('front_end', false);
+		$this->customer->init_columns_info('front_end');
+		$this->customer->validation->focusPrefix = null;
+
+		if(!post('first_name'))
+			throw new Phpr_ApplicationException('Please enter a first name.');
+		else if(!post('last_name'))
+			throw new Phpr_ApplicationException('Please enter a last name.');
+		
+		$post['password'] = null;
+
+		$this->customer->validation->getRule('email')->focusId('email');
+		$this->customer->save($post);
+	}
+
+	private function update_billing($post) {
+		
+		$post['password'] = null;
+		
+		$original_post = $post;
+		
+		$_POST = array_merge($post, $original_post['billing']);
+		$_POST['email'] = $this->customer->email;
+		
+		Shop_CheckoutData::set_billing_info(null);
+		
+		$billing_info = Shop_CheckoutData::get_billing_info();
+		
+		$this->customer->company = $billing_info->company;
+		$this->customer->billing_country_id = $billing_info->country;
+		$this->customer->billing_state_id = $billing_info->state;
+		$this->customer->billing_street_addr = $billing_info->street_address;
+		$this->customer->billing_city = $billing_info->city;
+		$this->customer->billing_zip = $billing_info->zip;
+		$this->customer->phone = $billing_info->phone;
+		$this->customer->password = null;
+		$this->customer->save();
+	}
+		
+	private function update_shipping($post) {
+		
+		$original_post = $post;
+		
+		$_POST = array_merge($post, $original_post['shipping']);
+		
+		Shop_CheckoutData::set_shipping_info();
+		Shop_CheckoutData::get_shipping_info()->save_to_customer($this->customer);
+		$this->customer->password = null;
+		$this->customer->save();
+	}	
+
 }
